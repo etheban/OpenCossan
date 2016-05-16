93c151f27723b7199e1db89ab3022de9e4395d7a
epatelli <epatelli@d4cb4665-ce59-4738-b46c-cc184f84e4af>
2016-05-16 16:32:28 +0000

    * Fix bug for setGridProperties with PerformanceFunction

--- COSSANXengine/src/reliability/@ProbabilisticModel/setGridProperties.m
+++ COSSANXengine/src/reliability/@ProbabilisticModel/setGridProperties.m
@@ -5,10 +5,28 @@ function Xobj = setGridProperties(Xobj,varargin)
 % The second setting for the grid will be ignored since they refer to the
 % PerformanceFunction
 %
-% ==================================================================
-% COSSAN-X - The next generation of the computational stochastic analysis
-% University of Innsbruck, Copyright 1993-2011 IfM
-% ==================================================================
+%
+% Author:  Edoardo Patelli and Matteo Broggi.
+% COSSAN Working Group
+% email address: openengine@cossan.co.uk
+% Website: http://www.cossan.co.uk
+
+% =====================================================================
+% This file is part of openCOSSAN.  The open general purpose matlab
+% toolbox for numerical analysis, risk and uncertainty quantification.
+%
+% openCOSSAN is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License.
+%
+% openCOSSAN is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with openCOSSAN.  If not, see <http://www.gnu.org/licenses/>.
+% =====================================================================
 
 % Argument Check
 OpenCossan.validateCossanInputs(varargin{:})
@@ -47,7 +65,7 @@ Nevaluators=length(Xobj.Xmodel.Xevaluator.CXsolvers);
 
 if exist('CevaluatorNames','var')
     for n=1:Nevaluators
-        if ~strcmp(CevaluatorNames{n},Xobj.Xevaluator.CSnames{n})
+        if ~strcmp(CevaluatorNames{n},Xobj.Xmodel.Xevaluator.CSnames{n})
             warning('openCOSSAN:Model:setGridProperties', ...
                 ['Name of the evaluator required %s does not correspond to the name' ...
                 ' present in the evaluator %s'],CevaluatorNames{n},Xobj.Xmodel.Xevaluator.CSnames{n})
@@ -108,6 +126,5 @@ if exist('LremoteInjectExtract','var')
     Xobj.Xmodel.Xevaluator.LremoteInjectExtract = LremoteInjectExtract;
 end
 
-varargout=Reliability_anal.setGridProperties('Vconcurrent',[1 1])
 end
 
